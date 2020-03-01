04d72be8f35562bfe7717f4296a47b7a6b71a5b8
clarkdkyle <clarkdkyle@gmail.com>
2020-02-29 20:54:16 -0600

    ports and gets

--- Develop/server.js
+++ Develop/server.js
@@ -2,74 +2,32 @@
 // =============================================================
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// const tables = [];
-// const waitlist = [];
-
-// const guest = {
-//   customerName: '',
-//   phoneNumber: '',
-//   customerEmail: '',
-//   customerID: '',
-// };
-
-// // Routes
-// // =============================================================
-
-// // Basic route that sends the user first to the AJAX Page
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'main.html'));
-// });
-
-// app.get('/main.html', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'main.html'));
-// });
-
-// app.get('/tables.html', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'tables.html'));
-// });
-
-// app.get('/reservations.html', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'reservations.html'));
-// });
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-// app.get('/api/tables', (req, res) => {
-//   return res.json(tables);
-// });
+// Routes
+// =============================================================
 
-// app.get('/api/waitlist', (req, res) => {
-//   return res.json(waitlist);
-// });
+// Basic route that sends the user first to the AJAX Page
 
-// app.post('/api/clear', (req, res) => {
-//   tables.empty();
-//   return res.json(clear);
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
-// app.post('/api/tables', (req, res) => {
-//   const data = req.body;
-  
-//   if (tables.length < 6) {
-//     tables.push(data);
-//   }   else {
-//     waitlist.push(data);
-//   }
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
-//   return res.json(data);
-// });
 
-// // Starts the server to begin listening
-// // =============================================================
-// app.listen(PORT, () => {
-//   console.log(`App listening on PORT ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
